12cef0e3a7d47b5003dcd09a4c844de1e3ae585e
pratiksha950 <salunkepratiksha950@gmail.com>
2025-10-30 21:36:08 +0530

    Sum of even and odd given by user:

---
+++ main.java
@@ -0,0 +1,28 @@
+// *************Sum of even and odd given by user**************
+// import java.util.*;
+// public class main {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int evenSum=0;
+//         int oddSum=0;
+//         int choice;
+        
+//         do{
+//             int num=sc.nextInt();
+//             if(num%2==0){
+//                 evenSum+=num;
+//             }else{
+//                 oddSum+=num;
+//             }
+//             System.out.println("wants to contine press 1");
+//              choice=sc.nextInt();
+           
+//         }while(choice==1);
+
+//         System.out.println("EvenSum :"+evenSum);
+//         System.out.println("Odd sum :"+oddSum);
+//     }}
+
+
+
+
