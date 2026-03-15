391cbda3c7373c5133556cd36fd2f6a396ba24e3
pratiksha950 <salunkepratiksha950@gmail.com>
2026-03-15 20:08:51 +0530

    Insersion quetion solved :

--- Sorting.java
+++ Sorting.java
@@ -26,25 +26,48 @@
 // }
 
 // ************selection sort********************
-public class Sorting {
-    public static void Selection(int arr[]) {
+// public class Sorting {
+//     public static void Selection(int arr[]) {
 
-    for(int i=0;i<arr.length-1;i++){
-        int minPos=i;
-        for(int j=i+1;j<arr.length;j++){
-            if(arr[minPos]>arr[j]){
-                minPos=j;
+//     for(int i=0;i<arr.length-1;i++){
+//         int minPos=i;
+//         for(int j=i+1;j<arr.length;j++){
+//             if(arr[minPos]>arr[j]){
+//                 minPos=j;
+//             }
+//         }
+//         int temp=arr[minPos];
+//         arr[minPos]=arr[i];
+//         arr[i]=temp;
+//     }
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[]={5,2,3,4,1};
+//         Selection(arr); 
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i] +" ");
+//         }
+//     }
+// }
+
+// ************Insersion sort********************
+public class Sorting {
+    public static void Insersion( int arr[]) {
+        for(int i=1;i<=arr.length-1;i++){
+            int cur=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>cur){
+                arr[prev+1]=arr[prev];
+                prev--;
             }
+            arr[prev+1]=cur;
         }
-        int temp=arr[minPos];
-        arr[minPos]=arr[i];
-        arr[i]=temp;
-    }
     }
 
     public static void main(String[] args) {
         int arr[]={5,2,3,4,1};
-        Selection(arr); 
+        Insersion(arr);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] +" ");
         }
