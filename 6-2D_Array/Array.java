5d899821a4766c69faa42b24bd5694e1e71a0e5d
pratiksha950 <salunkepratiksha950@gmail.com>
2026-03-20 06:54:28 +0530

    Digonal Sum program added:

--- 6-2D_Array/Array.java
+++ 6-2D_Array/Array.java
@@ -60,47 +60,67 @@
 // }
 
 // ******Spiral Matrix code*******
+// import java.util.*;
+// public class Array {
+//     public static void Search(int matrix[][]) {
+//         int startrow=0;
+//         int endrow=matrix.length-1;
+//         int startcol=0;
+//         int endcol=matrix[0].length-1;
+
+//         while(startrow<=endrow && startcol<=endcol){
+//             //top
+//             for(int j=startcol;j<=endcol;j++){
+//                 System.out.print(matrix[startrow][j]+" ");
+//             }
+//             //right
+//             for(int i=startrow+1;i<=endrow;i++){
+//                 System.out.print(matrix[i][endcol]+" ");
+//             }
+//             //BOTTOM
+//             if (startrow < endrow) {
+//                 for (int j = endcol - 1; j >= startcol; j--) {
+//                     System.out.print(matrix[endrow][j] + " ");
+//                 }
+//             }
+
+//             // LEFT
+//             if (startcol < endcol) {
+//                 for (int i = endrow - 1; i > startrow; i--) {
+//                     System.out.print(matrix[i][startcol] + " ");
+//                 }
+//             }
+
+//         startcol++;
+//         startrow++;
+//         endcol--;
+//         endrow--;
+//     }
+//     System.out.println();
+//     }
+//     public static void main(String[] args) {
+//        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+//        Search(matrix);
+// }
+// }
+
+
+// ***Digonal Sum***
 import java.util.*;
 public class Array {
-    public static void Search(int matrix[][]) {
-        int startrow=0;
-        int endrow=matrix.length-1;
-        int startcol=0;
-        int endcol=matrix[0].length-1;
-
-        while(startrow<=endrow && startcol<=endcol){
-            //top
-            for(int j=startcol;j<=endcol;j++){
-                System.out.print(matrix[startrow][j]+" ");
+    public static int Digonal(int matrix[][]) {
+        int sum=0;
+        for(int i=0;i<matrix.length;i++){         
+            sum+=matrix[i][i];
+            if(i!=matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
             }
-            //right
-            for(int i=startrow+1;i<=endrow;i++){
-                System.out.print(matrix[i][endcol]+" ");
-            }
-            //BOTTOM
-            if (startrow < endrow) {
-                for (int j = endcol - 1; j >= startcol; j--) {
-                    System.out.print(matrix[endrow][j] + " ");
-                }
-            }
-
-            // LEFT
-            if (startcol < endcol) {
-                for (int i = endrow - 1; i > startrow; i--) {
-                    System.out.print(matrix[i][startcol] + " ");
-                }
-            }
-
-        startcol++;
-        startrow++;
-        endcol--;
-        endrow--;
-    }
-    System.out.println();
+        }
+        return sum;
     }
     public static void main(String[] args) {
        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-       Search(matrix);
+       int res=Digonal(matrix);
+       System.out.println(res);
 }
 }
-
