093c1269febea92b05074ce8eb3087137c1eb536
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-14 16:15:07 +0530

    two sum problem solved:

--- 14-ArrayList/ArrayListDemo.java
+++ 14-ArrayList/ArrayListDemo.java
@@ -200,8 +200,8 @@ public class ArrayListDemo {
     }
 }*/
 
-//###Using Two-Pointer Method
-import java.util.ArrayList;
+//###Using Two-Pointer Method O(n)
+/*import java.util.ArrayList;
 public class ArrayListDemo {
     public static int StoreWater(ArrayList<Integer> list) {
         int maxWater = 0;
@@ -233,5 +233,110 @@ public class ArrayListDemo {
         list.add(7); 
         System.out.print(StoreWater(list));
     }
+}*/
+
+/*Two sum problem Brute force -O(N2)
+Find if any pair in a Sorted ArrayList has a target sum.
+list = [1, 2, 3, 4, 5, 6],target = 5
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static boolean StoreWater(ArrayList<Integer> list ,int target) {
+       for(int i=0;i<list.size();i++){
+        for(int j=i+1;j<list.size();j++){
+            if((list.get(i)+list.get(j))==target){
+                return true;
+            }
+        }
+       }
+       return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();//height
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        int target=5;
+        System.out.print(StoreWater(list,target));
+    }
+}*/
+
+/*Two sum problem Using Two Pointer
+Find if any pair in a Sorted ArrayList has a target sum.
+list = [1, 2, 3, 4, 5, 6],target = 5
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static boolean StoreWater(ArrayList<Integer> list ,int target) {
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp<rp){
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+
+             if(list.get(lp)+list.get(rp)<target){
+                lp++;;
+            }else{
+                rp--;
+            }
+        }
+        return false;
+       }
+      
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();//height
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        
+        int target=5;
+        System.out.print(StoreWater(list,target));
+    }
+}*/
+
+
+/*Two sum problem Using Two Pointer
+Find if any pair in a Sorted ArrayList has a target sum.
+list = [1, 2, 3, 4, 5, 6],target = 5 */
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static boolean StoreWater(ArrayList<Integer> list ,int target) {
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp<rp){
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+
+             if(list.get(lp)+list.get(rp)<target){
+                lp++;;
+            }else{
+                rp--;
+            }
+        }
+        return false;
+       }
+      
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();//height
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        
+        int target=5;
+        System.out.print(StoreWater(list,target));
+    }
 }
+
 
