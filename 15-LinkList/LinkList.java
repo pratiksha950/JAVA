84e93e743c7f6de770b33797088c64ec7df81883
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-14 23:37:35 +0530

    Printing LinkList program added:

---
+++ 15-LinkList/LinkList.java
@@ -0,0 +1,72 @@
+
+public class LinkList {
+
+    public static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        //creating node
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        //newNode next=head
+        newNode.next = head;
+        //head=newNode
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        //creating node
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        //newNode next=tail
+        tail.next = newNode;
+        //tail=newNode
+        tail = newNode;
+    }
+
+    //Printing LinkList
+    public void print() {
+        if (head == null) {
+            System.out.println("link list is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkList ll = new LinkList();
+        ll.print();
+        ll.addFirst((2));
+        ll.print();
+        ll.addFirst((1));
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+    }
+}
