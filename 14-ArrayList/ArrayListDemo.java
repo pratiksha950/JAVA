076ef4b4267d6b8e633ebb8a6e9c7f0200b4eeca
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-14 08:58:15 +0530

    find max number:

--- 14-ArrayList/ArrayListDemo.java
+++ 14-ArrayList/ArrayListDemo.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+ /*###import java.util.ArrayList;
 public class ArrayListDemo {
     public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
@@ -31,6 +31,54 @@ public class ArrayListDemo {
     list.add(0,7);
     System.out.println(list);
 
+    //size
+    System.out.println("size is :"+ list.size());
+    for(int i=0;i<list.size();i++){
+        System.out.print(list.get(i)+ " ");
+    }
+}
+} */
+
+//###Reverse an array
+/*import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println("before reverse"+ list);
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.print(list.get(i));
+
+        }
         
 }
 }
+*/
+
+//###Find max value
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(8);
+        list.add(99);
+        list.add(3);
+        list.add(4);
+        System.out.println("before reverse"+ list);
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            // if(list.get(i)>max){
+            //     max=list.get(i);
+            // }  
+            //Or
+            max=Math.max(max,list.get(i));
+        }
+        
+        System.out.println(max);
+}
+}
+
+
