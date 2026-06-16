490c074aab90a2b6857a0117a7976b4199881a8b
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-16 23:26:55 +0530

    required changes added:

---
+++ 15-LinkList/ZigZag.java
@@ -0,0 +1,16 @@
+/* ### Zig-Zag Linked ListFor a linked list of the form :
+L(1)->L(2)->L(3)->L(4) ... L(n-1)->L(n)
+convert it into a zig-zag form i.e. :
+L(1)->L(n)->L(2)->L(n-1)->L(3)->L(n-2)... */
+
+public class ZigZag {
+    public void ZZ(){
+        //first mid
+
+        //reverse  second half 
+
+        //alt merge-zig-zag merge
+    }
+    public static void main(String[] args) {
+      
+}}
