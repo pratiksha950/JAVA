6b20519af7b24c8097909a398a89312e640decd9
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-16 19:18:04 +0530

    removeCycle() code added:

--- 15-LinkList/Test.java
+++ 15-LinkList/Test.java
@@ -1,4 +1,4 @@
-//detect loop or cycle in linkedslist
+/*//###detect loop or cycle in linkedslist
 public class Test {
     public static class Node{
         int data;
@@ -22,6 +22,7 @@ public class Test {
         }
         return false;
     }
+
     public static void main(String[] args) {
     
         head=new Node(1);
@@ -31,5 +32,88 @@ public class Test {
         head.next.next.next.next = head.next;
 
         System.out.println(isCyclic());
+    }
+}
+*/
+
+
+ //###detect loop or cycle in linkedslist
+public class Test {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    static Node head;
+     public static void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    //detect cycle
+    public static boolean isCyclic(){//floyd's cycle algorithum
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;//detect loop or cycle
+            }
+        }
+        return false;
+    }
+
+    //Remove loop or cycle in linkedslist
+    public static void removeCycle(){
+        //detect cycle
+        Node slow=head;
+        Node fast=head;
+        boolean cycle=false;
+        while(fast!=null  && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                cycle=true;   
+                break; 
+            }
+        }
+        if(cycle==false){
+                return;
+            }
+
+        //finding meeting point
+        slow=head;
+        Node prev=null;
+        while(slow != fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        //remove cycle
+        prev.next=null;
+    }
+    public static void main(String[] args) {
+    
+        head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next=new Node(4);
+        head.next.next.next.next = head.next;
+
+       
+        System.out.println(isCyclic());
+        removeCycle();
+         System.out.println(isCyclic());
+         print();
     }
 }
