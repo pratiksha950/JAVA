dd5e05b6048e0099d81854da66dcc690c14a1688
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-17 09:08:28 +0530

    zig zag code added:

--- 15-LinkList/ZigZag.java
+++ 15-LinkList/ZigZag.java
@@ -4,13 +4,94 @@ convert it into a zig-zag form i.e. :
 L(1)->L(n)->L(2)->L(n-1)->L(3)->L(n-2)... */
 
 public class ZigZag {
-    public void ZZ(){
-        //first mid
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
 
+    public void addFirst(int data) {
+        //creating node
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        //newNode next=head
+        newNode.next = head;
+        //head=newNode
+        head = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("link list is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public void ZZ(){
+
+        //first mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast!=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
         //reverse  second half 
+        Node prev = null;
+        Node curr= mid.next;
+        mid.next =null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next =prev;
+            prev = curr;
+            curr = next;
+
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node NextL, NextR;
 
         //alt merge-zig-zag merge
+        while(left!=null && right!=null){
+            NextL = left.next;
+            left.next = right;
+            NextR = right.next;
+            right.next = NextL;
+
+            left = NextL;
+            right = NextR;
+        }
     }
     public static void main(String[] args) {
-      
+        ZigZag ll = new ZigZag();
+        ll.addFirst((1));
+        ll.addFirst((2));
+        ll.addFirst((3));
+        ll.addFirst((4));
+        ll.addFirst((5));
+        ll.addFirst((6));
+        ll.print();
+        ll.ZZ();
+        ll.print();
 }}
