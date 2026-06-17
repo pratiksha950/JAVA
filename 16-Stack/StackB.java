a60d9eaffab68683c13038f06b9092fa2d077cc6
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-17 22:46:16 +0530

    StockSpan problem solve:

--- 16-Stack/StackB.java
+++ 16-Stack/StackB.java
@@ -128,7 +128,7 @@ public class StackB {
 
 } */
 
-/* ### Push at bottom of stack */
+/* ### Push at bottom of stack 
 import java.util.*;
 public class StackB {
     public static void pushAtBottom(Stack<Integer> s, int data){
@@ -157,5 +157,71 @@ public class StackB {
         }
     }
 
-}
+}*/
 
+/* ### Reverse string using stack
+import java.util.*;
+public class StackB {
+    public static String reverseString(String str){
+       Stack<Character> s=new Stack<>();
+       int idx = 0;
+       while(idx < str.length()){
+        s.push(str.charAt(idx));
+        idx++;
+       }
+       StringBuilder result = new StringBuilder();
+        while(!s.isEmpty()){
+           char curr=s.pop();
+           result.append(curr); 
+        }
+        return result.toString();
+
+    }
+    public static void main(String args[]){
+        String str="abc";
+      
+        String Result = reverseString(str);
+        System.out.println(Result);
+       
+    }
+
+} */
+
+
+/* ### Reverse a stack */
+import java.util.*;
+public class StackB {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top=s.pop();
+        reverseStack(s);
+        pushAtBottom(s,top);
+    }
+
+    public static void printStack(Stack<Integer> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+    public static void main(String args[]){
+        Stack<Integer> s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+       reverseStack(s);
+        printStack(s);
+    }
+
+}
