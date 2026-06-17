6e7b6012841def9c68395f7e797cadf0c22188cd
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-17 18:23:20 +0530

    Push at bottom of stack:

--- 16-Stack/StackB.java
+++ 16-Stack/StackB.java
@@ -106,7 +106,7 @@ public class Stack {
 }
 */
 
-/* ### Stack using Collection framework */
+/* ### Stack using Collection framework
 import java.util.*;
 public class StackB {
 
@@ -126,4 +126,36 @@ public class StackB {
         }
     }
 
+} */
+
+/* ### Push at bottom of stack */
+import java.util.*;
+public class StackB {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void main(String args[]){
+        Stack<Integer> s =new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+       
+        // System.out.print(s.peek());
+        pushAtBottom(s,9);
+
+        while(! s.isEmpty()){
+            System.out.print(s.peek());
+           s.pop();
+        }
+    }
+
 }
+
