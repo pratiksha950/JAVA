8cbac3ae4775db4379d6279501b6939358667dc4
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-17 17:06:50 +0530

    Stack using LinkedList code added:

--- 16-Stack/Stack.java
+++ 16-Stack/Stack.java
@@ -1,3 +1,4 @@
+/* ### Stack using Arraylist
 import java.util.ArrayList;
 public class Stack {
     public static class StackDemo{
@@ -40,6 +41,66 @@ public class Stack {
         while(! s.isEmpty()){
             System.out.print(s.peek());
            s.Pop();
+        }
+    }
+}
+*/
+
+/* ### Stack using LinkedList */
+public class Stack {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static class StackDemo{
+        static Node head = null;
+        public static boolean isEmpty(){
+            return head == null;
+        }
+        //push
+        public static void Push(int data){
+            Node newNode = new Node(data);
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+        //peek
+        public static int peek(){
+             if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String args[]){
+        StackDemo s =new StackDemo();
+        s.Push(1);
+        s.Push(2);
+        s.Push(3);
+        s.Push(4);
+        s.Push(5);
+       
+        // System.out.print(s.peek());
+
+        while(! s.isEmpty()){
+            System.out.print(s.peek());
+           s.pop();
         }
     }
 }
