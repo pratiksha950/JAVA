2cd7dec6d72e9d5b3c5deb30d70560f4a5519d06
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-18 22:20:59 +0530

    Max Area in Histogram code added:

--- 16-Stack/Quetions.java
+++ 16-Stack/Quetions.java
@@ -83,7 +83,7 @@ Examples
 (((a + (b))) + (c + d)) → true
 ((((a) + (b)) + c + d)) → true
 ((a + b) + (c + d)) → false
-(((a + b)) + c) → true */
+(((a + b)) + c) → true 
 
 import java.util.Stack;
 public class Quetions {
@@ -118,5 +118,62 @@ public class Quetions {
         String str2="(a-b)";
         System.out.println(isDuplicate(str2));
         System.out.println(isDuplicate(str));
+}
+}*/
+
+/*Max Area in Histogram
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram 
+heights = [2,1,5,6,2,3] */
+
+import java.util.Stack;
+public class Quetions {
+    
+    public static void maxArea(int arr[]){
+       int maxArea =0;
+       int nsr[] = new int[arr.length];
+       int nsl[] = new int[arr.length];
+       //Next Smaller Right
+       Stack<Integer> s=new Stack<>();
+
+       for(int i=arr.length-1; i>=0; i--){
+        while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+            s.pop();
+        }
+        if(s.isEmpty()){
+            nsr[i]=arr.length;
+        }else{
+            nsr[i]=s.peek();
+        }
+        s.push(i);
+       }
+       //Next Smaller Right
+       s=new Stack<>();
+
+       for(int i=0; i<arr.length; i++){
+        while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+            s.pop();
+        }
+        if(s.isEmpty()){
+            nsl[i]=-1;
+        }else{
+            nsl[i]=s.peek();
+        }
+        s.push(i);
+       }
+       //current area
+       //width=nsr[i]-nsl[i]-1
+       for(int i=0 ; i<arr.length; i++){
+         int height = arr[i];
+         int width = nsr[i]-nsl[i]-1;
+         int currArea = height * width;
+         maxArea = Math.max(currArea , maxArea);
+       }
+
+       System.out.print("max area in histogram is = "+ maxArea);
+
+    }
+    public static void main(String[] args) {
+        int arr[]={2,1,5,6,2,3};
+        maxArea(arr);
 }
 }
