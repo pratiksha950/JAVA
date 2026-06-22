3bd3a52c16d4fe26179bd37b7b8c841ae70d334e
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-22 16:21:51 +0530

    Interleaves 2 halves of a queue code added:

--- 17-Queue/test.java
+++ 17-Queue/test.java
@@ -366,7 +366,7 @@ public class test {
     }
 }*/
 
-/*###First non reapeating letter */
+/*###First non reapeating letter 
 import java.util.*;
 public class test {
     public static void printNonReapeating(String str){
@@ -392,5 +392,40 @@ public class test {
     public static void main(String[] args) {
         String str = "aabccxb";
          printNonReapeating(str);
+    }
+}*/
+    
+/*Interleaves 2 halves of a queue O(n) */
+import java.util.*;
+public class test {
+    public static void interLeave(Queue<Integer> q){
+        Queue<Integer> firstHalf =new LinkedList<>();
+        int size = q.size();
+
+        for(int i=0 ; i< size/2 ;i++){
+            firstHalf.add(q.remove());
+        }
+        while(!firstHalf.isEmpty()){
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        interLeave(q);
+        while(!q.isEmpty()){
+            System.out.print(q.remove() + " ");
+        }
+        System.out.println();
     }
 }
