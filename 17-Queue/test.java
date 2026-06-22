08e441f505b05f4e2408cbad5ab39d58bcd82338
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-22 17:33:14 +0530

    Stack and Queue using Deque code added:

--- 17-Queue/test.java
+++ 17-Queue/test.java
@@ -474,7 +474,7 @@ public class test {
     }
 }*/
 
-/*### Stack using Deque */
+/*### Stack and using Deque */
 import java.util.*;
 
 public class test {
@@ -490,6 +490,19 @@ public class test {
             return d.getLast();
         }
     }
+
+    static class Queue{
+        Deque<Integer> d = new LinkedList<>();
+        public void add(int data){
+            d.addLast(data);
+        }
+        public int remove(){
+            return d.removeFirst();
+        }
+        public int peek(){
+            return d.getFirst();
+        }
+    }
     public static void main(String[] args) {
         Stack s = new Stack();
         s.push(1);
@@ -499,5 +512,17 @@ public class test {
         System.out.println(s.pop());
         System.out.println(s.pop());
         System.out.println(s.pop());
+
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println("peek = " + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+      
+        
+
     }
 }
