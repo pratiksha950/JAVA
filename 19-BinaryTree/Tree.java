510a7d337ff425343f6d2905b3857c6e369bba2e
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-25 17:02:54 +0530

    //InOrder TC-O(n) code added:

--- 19-BinaryTree/Tree.java
+++ 19-BinaryTree/Tree.java
@@ -34,6 +34,15 @@ public class Tree{
             preOrder(root.left);
             preOrder(root.right);
         }
+         //InOrder TC-O(n)
+        public static void inOrder(Node root){
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -41,6 +50,7 @@ public class Tree{
         Node root = bt.buildTree(nodes);
       
 
-        bt.preOrder(root);
+        // bt.preOrder(root);
+        bt.inOrder(root);
     }
 }
