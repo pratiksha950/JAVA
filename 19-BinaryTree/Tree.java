ff01e5bf72ebdd6975945f66ca20d29b956703ac
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-25 16:49:03 +0530

    PreOrder TC-O(n) code added:

--- 19-BinaryTree/Tree.java
+++ 19-BinaryTree/Tree.java
@@ -24,11 +24,23 @@ public class Tree{
 
             return newNode;
         }
+
+        //PreOrder TC-O(n)
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree bt = new BinaryTree();
         Node root = bt.buildTree(nodes);
-        System.out.println(root.data);
+      
+
+        bt.preOrder(root);
     }
 }
