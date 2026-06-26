f662b59be5d4480608ed985d0c2b8590def8dc12
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-26 10:12:56 +0530

    Diameter of tree- Approch 2-O(n) code added:

--- 19-BinaryTree/Part2.java
+++ 19-BinaryTree/Part2.java
@@ -1,4 +1,4 @@
-/*Dimeter of tree- Approch 1*/
+/*Dimeter of tree- Approch 1
 public class Part2 {
     public static class Node{
         int data;
@@ -52,4 +52,58 @@ public class Part2 {
 
         System.out.println(diameter(root));
     }
+}*/
+
+/*Diameter of tree- Approch 2-O(n)*/
+public class Part2 {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+          }
+    }
+  
+    public static class Info{
+        int diameter;
+        int height;
+
+        public Info(int diameter ,int height){
+            this.diameter = diameter;
+            this.height = height;
+        }
+    }
+
+    public static Info diameter(Node root){
+        if(root == null){
+            return new Info(0,0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diameter , rightInfo.diameter) , leftInfo.height + rightInfo.height +1);
+        int ht = Math.max(leftInfo.height , rightInfo.height) +1 ;
+
+        return new Info(diam , ht);
+    }
+  
+    public static void main (String[] args){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+
+        root.right.right = new Node(7);
+
+        System.out.println(diameter(root).diameter);
+    }
 }
+
