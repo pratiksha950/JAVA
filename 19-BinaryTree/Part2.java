f8339e624485b9131530c6f4dbf34e89091ba0e3
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-27 11:44:57 +0530

    Top view of tree code added:

--- 19-BinaryTree/Part2.java
+++ 19-BinaryTree/Part2.java
@@ -108,7 +108,8 @@ public class Part2 {
 }*/
 
 /*Question: Subtree of Another Tree
-Given the roots of two binary trees, root and subRoot, return true if there is a subtree of root with the same structure and node values as subRoot; otherwise, return false. */
+Given the roots of two binary trees, root and subRoot, return true if there is a subtree of root with the same structure and node values as subRoot; otherwise, return false.
+ */
 
 public class Part2{
     public static class Node{
@@ -186,3 +187,4 @@ public class Part2{
         
     }
 }
+
