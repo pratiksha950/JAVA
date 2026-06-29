d6e9591aa4c730a6b4895811e1ca8d6032c2c177
pratiksha950 <salunkepratiksha950@gmail.com>
2026-06-29 09:49:28 +0530

    Minimum distance between two nodes code added:

--- 19-BinaryTree/Part3.java
+++ 19-BinaryTree/Part3.java
@@ -119,8 +119,56 @@ public class Part3 {
 }
 */
 
-/*### Lowest common ancestor- approch-2*/
+/*### Lowest common ancestor- approch-2
+import java.util.ArrayList;
 
+public class Part3 {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+          }
+    }
+
+    public static Node lca2(Node root , int n1 , int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        if(rightLca == null){
+            return leftLca;
+        }
+         if(leftLca == null){
+            return rightLca;
+        }
+        return root;
+    }
+    
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+
+        root.right.right = new Node(7);
+
+        System.out.println(lca2( root , 4,7 ).data);
+    }
+}*/
+
+/*### Minimum distance between two nodes*/
 import java.util.ArrayList;
 
 public class Part3 {
@@ -154,6 +202,34 @@ public class Part3 {
         return root;
     }
 
+    public static int lcaDist(Node root , int n){
+        if(root == null){
+            return -1;
+        }
+
+        if(root.data == n){
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }else if(leftDist == -1){
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }   
+
+    public static int minDist(Node root , int n1 , int n2){
+        Node lca = lca2(root , n1 ,n2);
+        int dist1 = lcaDist(lca , n1);
+        int dist2 = lcaDist(lca , n2);
+
+        return dist1 + dist2;
+    }
     
 
     public static void main(String[] args) {
@@ -168,10 +244,10 @@ public class Part3 {
 
         root.right.right = new Node(7);
 
-       
-
-        System.out.println(lca2( root , 4,7 ).data);
+        System.out.println(minDist( root , 4,6 ));
     }
 }
+
+
 
 
