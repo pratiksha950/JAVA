e0f4f1412afd6b57444c8746391f56f2888c63a3
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-03 20:03:13 +0530

    Print root to leaf nodes code added:

--- 20-BinarySearchTree/Test.java
+++ 20-BinarySearchTree/Test.java
@@ -234,7 +234,7 @@ public class Test {
     }
 }*/
 
-/*### Print in Range- O(h)*/
+/*### Print in Range- O(h)
 public class Test {
 
     public static class Node{
@@ -327,7 +327,111 @@ public class Test {
         printInRange(root, 5, 10);
         System.out.println();
     }
+}*/
+
+/*### Print root to leaf nodes*/
+import java.util.*;
+public class Test {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean search(Node root , int key){
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == key){
+            return true;
+        }
+
+        if(root.data < key){
+            return search(root.right , key);
+        }else{
+            return search(root.left , key);
+        }
+    }
+
+     public static Node Insert(Node root , int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            //left subtree
+            root.left = Insert(root.left , val);
+        }else{
+            //right subtree
+            root.right = Insert(root.right , val);
+        }
+        return root;
+    }
+
+public static void printPath(ArrayList<Integer> path){
+    for(int i=0 ; i< path.size() ;i++){
+        System.out.print(path.get(i) + "->");
+    }
+    System.out.println("null");
 }
+
+  public static void printLeaf2Node(Node root , ArrayList<Integer> path){
+    if(root == null){
+        return;
+    }
+    path.add(root.data);
+    if(root.left == null && root.right == null){
+        printPath(path);
+    }
+    printLeaf2Node(root.left , path);
+    printLeaf2Node(root.right , path);
+    path.remove(path.size()-1);
+
+  }
+
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left); 
+        System.out.print(root.data + " ");
+        inOrder(root.right); 
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        int values[] = {8,5,3,1,4,6,10,11,14};
+        Node root = null;
+
+        for(int i=0 ; i<values.length ; i++){
+            root = Insert(root, values[i]);
+        }
+        inOrder(root);
+        System.out.println();
+
+        printLeaf2Node(root , new ArrayList<>());
+        System.out.println();
+    }
+}
+
+
 
 
 
