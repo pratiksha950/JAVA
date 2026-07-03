88123cb0818008aa387236ad36b37308a32e1e46
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-03 18:27:22 +0530

    Deleting Node in Binary search tree- O(h) code added:

--- 20-BinarySearchTree/Test.java
+++ 20-BinarySearchTree/Test.java
@@ -50,7 +50,7 @@ public class Test {
     }
 }*/
 
-/*### search in BST- O(h)*/
+/*### search in BST- O(h)
 public class Test {
 
     public static class Node{
@@ -122,4 +122,115 @@ public class Test {
              System.out.print("not found");
         }
     }
+}*/
+
+/*### Deleting Node in Binary search tree- O(h)*/
+public class Test {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean search(Node root , int key){
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == key){
+            return true;
+        }
+
+        if(root.data < key){
+            return search(root.right , key);
+        }else{
+            return search(root.left , key);
+        }
+    }
+
+     public static Node Insert(Node root , int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            //left subtree
+            root.left = Insert(root.left , val);
+        }else{
+            //right subtree
+            root.right = Insert(root.right , val);
+        }
+        return root;
+    }
+
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left); 
+        System.out.print(root.data + " ");
+        inOrder(root.right); 
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static Node delete(Node root , int val){
+        if(root.data < val){
+            root.right = delete(root.right , val);
+        }
+        else if(root.data > val){
+            root.left = delete(root.left , val);
+        }
+
+        else{ //voilla
+        //case 1:leaf node
+            if(root.left == null && root.right ==null){
+                return null;
+            }
+        
+        //case 2:one child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+        
+        //case 3:both children
+        Node IS = findInorderSuccessor(root.right);
+        root.data = IS.data;
+        root.right = delete(root.right , IS.data);
+
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int values[] = {8,5,3,1,4,6,10,11,14};
+        Node root = null;
+
+        for(int i=0 ; i<values.length ; i++){
+            root = Insert(root, values[i]);
+        }
+         inOrder(root);
+
+       root = delete(root, 10);
+       System.out.println();
+
+       inOrder(root);
+    }
 }
+
