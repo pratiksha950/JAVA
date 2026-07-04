ad419f173570dc7c8ba51e4bc95c4333df2179b9
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-04 08:48:33 +0530

    check is valid bst or not code added:

--- 20-BinarySearchTree/Test.java
+++ 20-BinarySearchTree/Test.java
@@ -329,7 +329,7 @@ public class Test {
     }
 }*/
 
-/*### Print root to leaf nodes*/
+/*### Print root to leaf nodes
 import java.util.*;
 public class Test {
 
@@ -415,7 +415,6 @@ public static void printPath(ArrayList<Integer> path){
         return root;
     }
 
-
     public static void main(String[] args) {
         int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
@@ -429,7 +428,93 @@ public static void printPath(ArrayList<Integer> path){
         printLeaf2Node(root , new ArrayList<>());
         System.out.println();
     }
+}*/
+
+
+/*### check is valid bst or not*/
+public class Test {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+     public static Node Insert(Node root , int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            //left subtree
+            root.left = Insert(root.left , val);
+        }else{
+            //right subtree
+            root.right = Insert(root.right , val);
+        }
+        return root;
+    }
+
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left); 
+        System.out.print(root.data + " ");
+        inOrder(root.right); 
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static boolean isValidBST(Node root , Node min , Node max){
+        if(root == null){
+            return true;
+        }
+
+        if(min!=null && root.data <= min.data){
+            return false;
+        }
+        else if(max!=null && root.data >= max.data){
+            return false;
+        }
+
+        return isValidBST(root.left , min , root) && isValidBST(root.right , root , max);
+    }
+
+
+    public static void main(String[] args) {
+        int values[] = {1,1,1};
+        Node root = null;
+
+        for(int i=0 ; i<values.length ; i++){
+            root = Insert(root, values[i]);
+        }
+        inOrder(root);
+        System.out.println();
+
+        if(isValidBST(root, null , null)){
+            System.out.println("valid");
+        }else{
+            System.out.println("Not valid");
+        }
+       
+    }
 }
+
 
 
 
