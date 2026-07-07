e91abb059e5ac6ad2d6b16d57415715324b3b508
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-07 18:42:13 +0530

    Insert in Heap code added:

--- 21-Heap/Test.java
+++ 21-Heap/Test.java
@@ -1,4 +1,4 @@
-/*### PriorityQueue concept*/
+/*### PriorityQueue concept
 import java.util.*;
 public class Test {
 
@@ -32,4 +32,37 @@ public class Test {
         }
 
     }
+}*/
+
+/*### Insert in Heap*/
+import java.util.*;
+public class Test {
+
+    static class Heap{
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data){
+            //add at last ind
+            arr.add(data);
+            int x = arr.size() - 1 ; //child idx
+            int par = (x-1)/2; //parent idx
+
+            while(arr.get(x) < arr.get(par)){  // O(logn)
+                //swap
+                int temp = arr.get(x);
+                arr.set(x , arr.get(par));
+                arr.set(par , temp);
+            }
+        }
+    }
+
+    public int peek(){
+        return arr.get(0);
+    }
+
+    public static void main(String[] args) {
+        
+
+    }
 }
+
