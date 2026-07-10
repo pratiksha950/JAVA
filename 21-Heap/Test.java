9f4a1ac94f5f6366cf6eea1b7cdf5bfbe568a0ce
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-10 10:09:57 +0530

    Connect N Ropes code added:

--- 21-Heap/Test.java
+++ 21-Heap/Test.java
@@ -180,7 +180,14 @@ public class Test {
 
 } */
 
-/* ### Nearby car problem */
+/* ### Nearby Cars
+We are given N points in a 2D plane which are locations of N cars. If we are at the origin (0, 0), print the nearest K cars.
+Cars:
+C0 (3, 3)
+C1 (5, -1)
+C2 (-2, 4)
+K = 2
+
 import java.util.*;
 
 public class Test {
@@ -217,5 +224,34 @@ public class Test {
         for(int i=0 ; i<k ;i++){
             System.out.println("C" + pq.remove().idx);
         }
+    }
+}*/
+
+/* ### Connect N Ropes: Given are N ropes of different lengths, the task is to connect these ropes into one rope with minimum cost, such that the cost to connect two ropes is equal to the sum of their lengths.
+ropes = {4, 3, 2, 6}
+Output:29
+ */
+
+import java.util.*;
+
+public class Test {
+       public static void main(String args[]) {
+        int ropes[] = {4,3,2,6};
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0 ; i<ropes.length ; i++){
+            pq.add(ropes[i]);
+        }
+
+        int cost = 0;
+        while(pq.size() > 1){
+            int min = pq.remove();
+            int min2 = pq.remove();
+
+            cost += min + min2;
+            pq.add(min + min2);
+        }
+        System.out.println("cost of connecting n ropes is = " + cost);
+        
     }
 }
