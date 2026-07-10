40a5dca294bef8868b74a48d853dbbcdcf8b8396
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-10 10:48:33 +0530

    Weakest Soldier code added:

--- 21-Heap/Test.java
+++ 21-Heap/Test.java
@@ -230,7 +230,7 @@ public class Test {
 /* ### Connect N Ropes: Given are N ropes of different lengths, the task is to connect these ropes into one rope with minimum cost, such that the cost to connect two ropes is equal to the sum of their lengths.
 ropes = {4, 3, 2, 6}
 Output:29
- */
+
 
 import java.util.*;
 
@@ -253,5 +253,67 @@ public class Test {
         }
         System.out.println("cost of connecting n ropes is = " + cost);
         
+    }
+} */
+
+/* ###Weakest Soldier :
+We are given an m × n binary matrix of 1s (soldiers) and 0s (civilians). The soldiers are positioned in front of the civilians. That is, all the 1s will appear to the left of all the 0s in each row.
+A row i is weaker than a row j if one of the following is true:
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Find the K weakest rows. 
+Example:
+m = 4, n = 4, k = 2
+Matrix:
+1 0 0 0
+1 1 1 1
+1 0 0 0
+1 0 0 0 */
+
+import java.util.*;
+
+public class Test {
+    static class Row implements Comparable<Row> {
+        int soldiers;
+        int idx;
+
+        public Row(int soldiers, int idx) {
+            this.soldiers = soldiers;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Row r2) {
+        if (this.soldiers == r2.soldiers) {
+            return this.idx - r2.idx;
+        } else {
+            return this.soldiers - r2.soldiers;
+        }
+    }
+    }
+    public static void main(String args[]) {
+        int army[][] = {
+            {1, 0, 0, 0},
+            {1, 1, 1, 1},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0},
+        };
+            int k=2;
+
+            PriorityQueue<Row> pq = new PriorityQueue<>();
+
+            for (int i = 0; i < army.length; i++) {
+            int count = 0;
+                    
+            for (int j = 0; j < army[0].length; j++) {
+                count += army[i][j] == 1 ? 1 : 0;
+            }
+        
+            pq.add(new Row(count, i));
+            }
+
+            for (int i = 0; i < k; i++) {
+             System.out.println("R" + pq.remove().idx);
+        }
     }
 }
