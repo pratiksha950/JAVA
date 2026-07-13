f458955cf3e63d7986a54fcafd3552303a3cb1b0
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-13 16:46:29 +0530

    Basic operations of hashMap added:

---
+++ 22-Hashing/Test.java
@@ -0,0 +1,33 @@
+/*Basic operations of hashMap */
+import java.util.HashMap;
+public class Test {
+    public static void main(String args[]){
+        HashMap<String , Integer> hm = new HashMap<>();
+
+        //insert - O(1)
+        hm.put("India" , 100);
+        hm.put("Us" , 80);
+        hm.put("Ambi" , 180);
+        System.out.println(hm);
+
+         //get - O(1)
+        int popuation = hm.get("India");
+        System.out.println(popuation);
+
+        //containsKey - O(1)
+        System.out.println(hm.containsKey("India"));//true
+        System.out.println(hm.containsKey("Indiqaaa"));//false
+
+        //remove -O(1)
+        System.out.println(hm.remove("Ambi"));
+        System.out.println(hm);
+
+        //size
+        System.out.println(hm.size());
+
+        //isEmpty
+        System.out.println(hm.isEmpty());
+        hm.clear();
+        System.out.println(hm.isEmpty());
+    }
+}
