3c9b3e9b1c16c8dc17432c02b4ecc0b35860d5c4
pratiksha950 <salunkepratiksha950@gmail.com>
2026-07-13 19:54:41 +0530

    Iteration on hashMap:

--- 22-Hashing/Test.java
+++ 22-Hashing/Test.java
@@ -1,4 +1,5 @@
-/*Basic operations of hashMap */
+/* ### Basic operations of hashMap 
+
 import java.util.HashMap;
 public class Test {
     public static void main(String args[]){
@@ -30,4 +31,28 @@ public class Test {
         hm.clear();
         System.out.println(hm.isEmpty());
     }
+}*/
+
+/* ### Iteration on hashMap */
+import java.util.*;
+public class Test {
+    public static void main(String args[]){
+        HashMap<String , Integer> hm = new HashMap<>();
+
+        //insert - O(1)
+        hm.put("India" , 100);
+        hm.put("Us" , 80);
+        hm.put("Ambi" , 180);
+        hm.put("Kamal" , 50);
+        hm.put("Belapur" , 16);
+
+        //Iterate
+        Set<String> keys = hm.keySet();
+        System.out.println(keys);
+       
+        for(String k : keys){
+            System.out.println("key = "+ k + ", value =" + hm.get(k));
+        }
+    }
 }
+
